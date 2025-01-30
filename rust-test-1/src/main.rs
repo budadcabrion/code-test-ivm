e337e7fcdd1199fd5a1b36764ec0f8418c1e426e
jan-karl <jan-karl@jan-karls-mbp.mynetworksettings.com>
2025-01-29 21:00:15 -0500

    initial commit

---
+++ rust-test-1/src/main.rs
@@ -0,0 +1,54 @@
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use std::sync::Mutex;
+
+struct AppState {
+    app_name: String,
+    counter: Mutex<i32>
+}
+
+async fn index(app_state: web::Data<AppState>) -> impl Responder {
+    *app_state.counter.lock().unwrap() += 1;
+
+    let app_name = &app_state.app_name;
+    HttpResponse::Ok().body(format!("Welcome to my web app {app_name}"))
+}
+
+#[get("/hello/{name}")]
+async fn hello(path: web::Path<String>, app_state: web::Data<AppState>) -> impl Responder {
+    *app_state.counter.lock().unwrap() += 1;
+
+    let name = path.into_inner();
+    HttpResponse::Ok().body(
+        format!("Welcome {}", name)
+    )
+}
+
+#[get("/counter")]
+async fn counter(app_state: web::Data<AppState>) -> impl Responder {
+    let mut c = app_state.counter.lock().unwrap();
+    *c += 1;
+
+    HttpResponse::Ok().body(
+        format!("Request count: {c}")
+    )
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let state = web::Data::new(AppState {
+        app_name: String::from("jan-karl's test app"),
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .route("/", web::get().to(index))
+            .service(hello)
+            .service(counter)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
